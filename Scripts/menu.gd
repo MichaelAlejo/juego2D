f84ea666305034ebc2fdd9ae62a1ea706299ae61
josepb80 <jbarrera@elpuig.xeill.net>
2026-02-23 13:21:05 +0100

    Tutorial y todo

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,12 +1,10 @@
 extends Control
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/1_map.scn")
-
+	get_tree().change_scene_to_file("res://Scenes/map1.scn")
 
 func _on_controls_pressed() -> void:
-	print("Controls Pressed")
-
+	get_tree().change_scene_to_file("res://Scenes/tutorial.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
