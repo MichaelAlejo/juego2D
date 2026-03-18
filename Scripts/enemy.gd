94018b34f67caf1eea942234159af41b2a400876
MichaelAlejo <mcastillo@elpuig.xeill.net>
2026-03-18 12:35:45 +0100

    nada

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,51 +1,52 @@
 extends CharacterBody2D
 
-@export var speed = 40
-var direction = 1
-var dead = false
+@export var speed := 50
+var direction := -1
 
-@onready var ray = $RayCast2D
+@onready var floor_check = $FloorCheck
 @onready var sprite = $AnimatedSprite2D
 
+var can_flip := true
+var flip_cooldown := 3.0
+var flip_timer := 0.0
 
 func _physics_process(delta):
-
-	if dead:
-		return
-
-	# gravedad
+	# Gravedad
 	if not is_on_floor():
 		velocity.y += 500 * delta
-
-	velocity.x = speed * direction
-
-	if not ray.is_colliding():
-		turn()
-
-	if is_on_wall():
-		turn()
-
+	
+	# Movimiento
+	velocity.x = direction * speed
+	
+	# Timer de flip
+	if not can_flip:
+		flip_timer -= delta
+		if flip_timer <= 0:
+			can_flip = true
+	
+	# Detectar suelo
+	if not floor_check.is_colliding() and can_flip:
+		flip()
+	
+	# Detectar pared
+	if is_on_wall() and can_flip:
+		flip()
+	
 	move_and_slide()
+	update_sprite()
 
-
-func turn():
+func flip():
 	direction *= -1
-	ray.position.x *= -1
-	sprite.flip_h = !sprite.flip_h
+	floor_check.position.x *= -1
+	
+	can_flip = false
+	flip_timer = flip_cooldown
 
-
-func _on_area_2d_body_entered(body):
-
-	if body.name == "Player":
-
-		# si el jugador viene desde arriba
-		if body.velocity.y > 0 and body.global_position.y < global_position.y:
-			die()
-			body.jump_bounce()
-
-		else:
-			body.die()
-
-
-func die():
-	queue_free()
+func update_sprite():
+	# Animación de caminar
+	if is_on_floor() and abs(velocity.x) > 0:
+		if sprite.animation != "1 - walk":
+			sprite.play("1 - walk")
+	
+	# Volteo del sprite
+	sprite.flip_h = direction > 0
