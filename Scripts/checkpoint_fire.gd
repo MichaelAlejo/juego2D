67e1b0d33d8b951a62a970d70cff53f5017b888c
josepb80 <jbarrera@elpuig.xeill.net>
2026-04-21 13:30:55 +0200

    luces

--- Scripts/checkpoint_fire.gd
+++ Scripts/checkpoint_fire.gd
@@ -2,8 +2,11 @@ extends Area2D
 
 func _ready():
 	$AnimatedSprite2D.play("orange_fire")
-
+	$PointLightOrange.visible = true
+	$PointLightBlue.visible = false
 
 func _on_body_entered(body: Node2D):
 	Checkpoint.last_position = global_position + Vector2(0, -20)
 	$AnimatedSprite2D.play("blue_fire")
+	$PointLightOrange.visible = false
+	$PointLightBlue.visible = true
